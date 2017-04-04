ac404d26bc939ed2c97af03c835470b807ee6526
Shobi <ppshobi@gmail.com>
2017-04-04 17:15:01 +0530

    favourites table migration updated

--- database/migrations/2017_04_04_114032_create_favourites_table.php
+++ database/migrations/2017_04_04_114032_create_favourites_table.php
@@ -15,6 +15,8 @@ class CreateFavouritesTable extends Migration
     {
         Schema::create('favourites', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->integer('post_id')->unsigned();
             $table->timestamps();
         });
     }
