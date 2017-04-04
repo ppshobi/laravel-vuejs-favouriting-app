231e51c3b123b4ac224d714ae560e645d80bd3b6
Shobi <ppshobi@gmail.com>
2017-04-04 18:25:57 +0530

    Bug fix not using the Post Model

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 
+use Illuminate\Http\Request;
+use App\Post;
 class PostsController extends Controller
 {
     public function index(){
